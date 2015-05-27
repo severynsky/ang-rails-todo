35a139557c5e371c2dd3ab8865e544223ca410ca
severynsky <severynr@gmail.com>
2015-05-27 18:23:56 +0300

    basic crud operation

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'haml', '~> 4.0.6'
 gem 'bootstrap-sass', '~> 3.3.4.1'
 gem 'pry', '~> 0.10.1'
+gem 'bootstrap_form'
+
 
 gem 'mysql', '~> 2.9.1'
 
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,46 @@
 class ListsController < ApplicationController
+  before_action :find_list, only: [:show, :edit, :update, :destroy]
 
   def index
-    binding.pry
+    @lists = List.all
   end
 
   def new
-
+    @list = List.new
   end
+
+  def create
+    @list = List.new(permitted_params)
+    if @list.save
+      redirect_to root_path, notice: "task has been created!"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    # binding.pry
+    if @list.update(permitted_params)
+      redirect_to root_path, notice: "task has been udpated!"
+    end
+  end
+
+  def destroy
+    if @list.destroy
+      flash[:notice] =  "item has been deleted!"
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def permitted_params
+    params.require(:list).permit(:body)
+  end
+
+  def find_list
+    @list = List.find(params[:id])
+  end
+
 end
