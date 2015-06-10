8d5a5dd841c8c62a4b45f72fa7f68ef547d3cde5
severynsky <severynr@gmail.com>
2015-06-10 14:34:31 +0300

    added templates to application rb

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module ToDo
 
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts", "templates")
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
   end
 end
