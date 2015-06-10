6722866c98ffc9c0cbf7d42b98f7f447b78a1390
severynsky <severynr@gmail.com>
2015-06-10 17:03:22 +0300

    small changes to make file format accesible

--- app/assets/javascripts/services/List.js
+++ app/assets/javascripts/services/List.js
@@ -1,7 +1,7 @@
 var todo = angular.module('todo');
 
 todo.factory('List',['$resource', function($resource){
-    return $resource('/lists/:id', {id: '@id'}, {
+    return $resource('/lists/:id.json', {id: '@id'}, {
         update: {
             method: 'PUT' //this method issues a PUT request
         },
